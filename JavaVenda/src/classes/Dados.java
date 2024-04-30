4d9a6a0061889953972aebb48d588118b8da05fe
Nebuhazard <sandro.25102000@gmail.com>
2024-04-30 17:59:24 -0300

    mudei o programa de Hotelaria para programa de Padaria!
    desisti da Hotelaria e decidi fazer um programa de Padaria

---
+++ JavaVenda/src/classes/Dados.java
@@ -0,0 +1,13 @@
+
+package classes;
+
+
+public class Dados {
+    public boolean validarUsuario (String usuario,String senha){
+        if(usuario.equals("san1")&& senha.equals("123")) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
